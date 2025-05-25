c1e3b511bb5991cecec55123b7e39a60ca49b84e
Tushar Singh <tusharsingh@lidolearning.com>
2025-05-26 01:39:35 +0530

    corrected json

--- api/serve.go
+++ api/serve.go
@@ -5,13 +5,23 @@ import (
     "net/http"
 )
 
-func main() {
-    gin.SetMode(gin.ReleaseMode)
-    r := gin.New()
-    r.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "Hello, World from Gin on Vercel!",
-        })
-    })
-    http.Handle("/", r)
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World from Gin on Vercel!",
+		})
+	})
+
+	// Serve the request using the Gin router
+	router.ServeHTTP(w, r)
 }
