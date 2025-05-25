9a173a18ad4e1aa696a5e3c7484551c92091b5f1
Tushar Singh <tusharsingh@lidolearning.com>
2025-05-26 02:28:19 +0530

    added go mod

--- api/serve.go
+++ api/serve.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 )
 
-// Vercel looks for this exported Handler
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
